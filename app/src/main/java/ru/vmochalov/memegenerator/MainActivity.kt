7b9be980ef2795cd3a56dee34aadd091dac024ac
Vladimir Mochalov <mochalovv@yandex.ru>
2019-09-17 08:29:35 +0300

    started implementing screens

---
+++ app/src/main/java/ru/vmochalov/memegenerator/MainActivity.kt
@@ -0,0 +1,15 @@
+package ru.vmochalov.memegenerator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Vladimir Mochalov on 15.09.2019.
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/ru/vmochalov/memegenerator/ui/imageselection/ImageSelectionFragment.kt
@@ -0,0 +1,7 @@
+package ru.vmochalov.memegenerator.ui.imageselection
+
+/**
+ * Created by Vladimir Mochalov on 15.09.2019.
+ */
+class ImageSelectionFragment {
+}
