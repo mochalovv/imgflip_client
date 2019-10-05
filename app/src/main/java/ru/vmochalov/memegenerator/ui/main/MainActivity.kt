b0707f1408ee972f1eade0f860129fc14385bc96
Vladimir Mochalov <mochalovv@yandex.ru>
2019-10-05 17:16:51 +0300

    implemented Labels screen and Pm; added navigation between screens

--- app/src/main/java/ru/vmochalov/memegenerator/ui/main/MainActivity.kt
+++ app/src/main/java/ru/vmochalov/memegenerator/ui/main/MainActivity.kt
@@ -3,15 +3,16 @@ package ru.vmochalov.memegenerator.ui.main
 import me.dmdev.rxpm.navigation.NavigationMessage
 import me.dmdev.rxpm.navigation.NavigationMessageHandler
 import ru.vmochalov.memegenerator.KoinHelper
+import ru.vmochalov.memegenerator.R
 import ru.vmochalov.memegenerator.extension.back
+import ru.vmochalov.memegenerator.extension.goTo
 import ru.vmochalov.memegenerator.extension.setRoot
-import ru.vmochalov.memegenerator.ui.Back
-import ru.vmochalov.memegenerator.ui.OpenAnythingScreen
-import ru.vmochalov.memegenerator.ui.OpenTemplateSelectionScreen
+import ru.vmochalov.memegenerator.ui.*
 import ru.vmochalov.memegenerator.ui.anything.AnythingScreen
 import ru.vmochalov.memegenerator.ui.common.BasePmActivity
-import ru.vmochalov.memegenerator.R
 import ru.vmochalov.memegenerator.ui.imageselection.ImageSelectionScreen
+import ru.vmochalov.memegenerator.ui.labels.LabelsScreen
+import timber.log.Timber
 
 class MainActivity : BasePmActivity<MainPm>(), NavigationMessageHandler {
 
@@ -31,6 +32,12 @@ class MainActivity : BasePmActivity<MainPm>(), NavigationMessageHandler {
             is OpenAnythingScreen -> router.setRoot(AnythingScreen.newInstance(message.number))
 
             is OpenTemplateSelectionScreen -> router.setRoot(ImageSelectionScreen())
+
+            is OpenLabelsScreen -> router.goTo(LabelsScreen())
+
+            is OpenResultScreen -> {
+                Timber.d("!! open result screen")
+            }
 
             is Back -> if (!router.back()) finish()
         }
