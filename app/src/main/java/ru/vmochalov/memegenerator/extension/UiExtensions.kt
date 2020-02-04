3db707d4514b6d5015281e46787bf024445ebb76
Vladimir Mochalov <mochalovv@yandex.ru>
2020-02-04 09:27:18 +0300

    moved Callback to a separate file; removed unused code

--- app/src/main/java/ru/vmochalov/memegenerator/extension/UiExtensions.kt
+++ app/src/main/java/ru/vmochalov/memegenerator/extension/UiExtensions.kt
@@ -7,16 +7,3 @@ import android.view.inputmethod.InputMethodManager
 fun View.visible(visible: Boolean, useGone: Boolean = true) {
     this.visibility = if (visible) View.VISIBLE else if (useGone) View.GONE else View.INVISIBLE
 }
-
-fun View.showKeyboard() {
-    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    if (requestFocus()) imm.showSoftInput(this, 0)
-}
-
-fun View.hideKeyboard(clearFocus: Boolean = false) {
-    if (clearFocus) {
-        clearFocus()
-    }
-    val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-    imm.hideSoftInputFromWindow(windowToken, 0)
-}
